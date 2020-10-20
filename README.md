34471e3ebe0eb86fb177802f21b47c66c1648c4e
Todd Warren <todd@warrenfamily.org>
2020-10-20 09:41:43 -0700

    basic processing functions
    - added results path
    - added use of Path runtime object
    - updated launch.json to output std out to terminal.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Class Grade Runner
+
+This is a class grade runner designed to be used with GitHub Classroom and the GitHub classroom desktop tool. It enable a batch command to be run on a set of directories; and uses the student csv file downloaded from github class room to build a new csv file of calibrated results based on the batch program.
--- runtests.ts
+++ runtests.ts
@@ -15,6 +15,8 @@ interface RunTestArgs {
   resultsCSV: string;
   command: string;
   path: string;
+  resultsPath: string;
+  isrepo: boolean;
 }
 export class RunTests implements YargCommand {
   constructor() {
@@ -29,12 +31,20 @@ export class RunTests implements YargCommand {
           type: "string",
           default: "student.csv",
           description: "file with student github id's and emails",
-          alias: ["student", "scsv"],
+          alias: ["students", "scsv"],
         },
         resultsCSV: {
           type: "string",
           default: "results.csv",
           description: "file to write and append results",
+          alias: ["rcsv"],
+        },
+        resultsPath: {
+          type: "string",
+          default: "_results",
+          description:
+            "path to directory where results for each command should be stored",
+          alias: ["rp"],
         },
         command: {
           type: "string",
@@ -45,6 +55,13 @@ export class RunTests implements YargCommand {
           type: "string",
           default: "*.*",
           description: "parent path of repos with results",
+          alias: ["p"],
+        },
+        isrepo: {
+          type: "binary",
+          default: true,
+          description:
+            "each directory should be a git repository (have a .git subdir)",
         },
       })
       .help(
@@ -67,18 +84,8 @@ export class RunTests implements YargCommand {
     return directories;
   }
   private isDir(path: string): boolean {
-    const globChars = /[\*\?]+/;
-    if (path.match(globChars) !== null) {
-      // is a glob not a dir
-      return false;
-    } else {
-      if (!existsSync(path)) {
-        return false;
-      } else {
-        // is it a dir or a file?
-        return this.getdirectories(path).length > 0;
-      }
-    }
+    const isDir = new Path(path);
+    return isDir.isDir;
   }
   private async performForDirectories(directories: WalkEntry[]): Promise<any> {}
   command(yargs: YargsType & RunTestArgs): YargsType {
@@ -86,12 +93,32 @@ export class RunTests implements YargCommand {
     console.log(`studentCSV: ${yargs["studentCSV"]}`);
     console.log(`resultsCSV: ${yargs.resultsCSV}`);
     console.log(`command: ${yargs.command}`);
-    const path = this.isDir(yargs.path) ? yargs.path + "/*" : yargs.path;
-    let directories = this.getdirectories(path);
+    // append *.* if it's a dir
+    const path = this.isDir(yargs.path)
+      ? new Path(yargs.path).push("*")
+      : new Path(yargs.path);
+    let directories = this.getdirectories(path.toString());
     if (directories.length == 0) {
       console.log(`nothing to do for ${path}`);
     } else {
-      console.log(`performing command in ${directories.length}`);
+      console.log(`performing command in ${directories.length} directories`);
+      const resultsPath = new Path(yargs.resultsPath);
+      if (!resultsPath.isDir) {
+        if (resultsPath.isFile) {
+          console.log(
+            `${resultsPath} is a file; a directory or new directory path was expected...terminating`
+          );
+          return yargs;
+        } else {
+          if (!resultsPath.mkDirSync(true)) {
+            console.log(`can't create the path ${resultsPath} terminating...`);
+            return yargs;
+          }
+        }
+      }
+      directories.forEach((dir) => {
+        console.log(`processing ${dir.name}`);
+      });
     }
     return yargs;
   }
